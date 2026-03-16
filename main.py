f37c6be39344700f4826fbaa7b70a38e76138801
aessom <aessom@pm.me>
2026-03-16 13:25:33 -0500

    Fixed scaling issues

--- main.py
+++ main.py
@@ -21,19 +21,35 @@ class CalculatorGUI:
     def build_gui(self):
         frame = ttk.Frame(self.root)
         frame.grid(column=0, row=0, sticky=NSEW)
+        top = frame.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        frame.rowconfigure([0, 1], weight=1)
 
         complete_line_label = ttk.Label(frame, textvariable=self.complete_line)
         current_line_label = ttk.Label(frame, textvariable=self.current_line)
 
-        complete_line_label.grid(row=0, column=0, columnspan=3, sticky=E)
-        current_line_label.grid(row=1, column=0, columnspan=3, sticky=E)
+        complete_line_label.grid(row=0, column=0, columnspan=4, sticky=E)
+        current_line_label.grid(row=1, column=0, columnspan=4, sticky=E)
+
+        complete_line_label.config(font=("TkTextFont", COMPLETE_LINE_FONT_SIZE))
+        current_line_label.config(font=("TkTextFont", CURRENT_LINE_FONT_SIZE))
 
         for row in range(len(GRID_LABELS)):
             for column in range(len(GRID_LABELS[row])):
                 button = ttk.Button(frame,
                                     text=str(GRID_LABELS[row][column]),
                                     command=partial(self.button_press, GRID_LABELS[row][column])
-                                    ).grid(column=column + BUTTON_OFFSET["x"], row=row + BUTTON_OFFSET["y"])  
+                                    )
+                button.grid(column=column + BUTTON_OFFSET["x"], row=row + BUTTON_OFFSET["y"], sticky=NSEW)
+                frame.rowconfigure(row + BUTTON_OFFSET["y"], weight=1)
+                frame.columnconfigure(column + BUTTON_OFFSET["x"], weight=1)
+
+    def resize_labels(self, root):
+        label_font = font.nametofont("TkTextFont")
+        label_font.config(size=int(root.width/10))
+        self.complete_line_label.config(font=label_font)
+        self.current_line_label.config(font=label_font)
 
     def button_press(self, button):
         if isinstance(button, int):
